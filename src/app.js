27f04580c2c75fa30cb32713ea44fd16ed722b26
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-02-24 12:05:53 +0000

    se creo html y jss de generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function() {}
   //write your code here
+document.getElementById('btn-excuse').addEventListener('click', function() {
+    let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+    let action = ["ate ", "peed ", "crushed ", "broke "];
+    let what = ["my homework ", "my phone ", "the car "];
+    let when = [
+      "before the class.",
+      "when I was sleeping.",
+      "while I was exercising.",
+      "during my lunch.",
+      "while I was praying."
+    ];
+  
+    let primeraPalabra = who[Math.floor(Math.random() * who.length)];
+    let segundaPalabra = action[Math.floor(Math.random() * action.length)];
+    let terceraPalabra = what[Math.floor(Math.random() * what.length)];
+    let cuartaPalabra = when[Math.floor(Math.random() * when.length)];
+  
+    document.getElementById("excuse").innerHTML =
+      primeraPalabra + segundaPalabra + terceraPalabra + cuartaPalabra;
+  });
   console.log("Hello Rigo from the console!");
-};
+
